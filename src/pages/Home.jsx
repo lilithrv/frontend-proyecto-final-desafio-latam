f825175e2553ebde4de26523b6242da670ce62b8
Leslie Reyes <lesliereyesv@gmail.com>
2023-09-08 14:52:22 -0300

    add loading at home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,15 @@ import BookCarousel from "../components/BookCarousel"
 const Home = () => {
     const [latestBooks, setLatestBooks] = useState([]);
     const [popularBooks, setPopularBooks] = useState([]);
-
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         const fetchLatestBooks = async () => {
             try {
+                setLoading(true)
                 const response = await fetch(`${import.meta.env.VITE_BASE_URL}books/latest`);
                 const data = await response.json();
+                setLoading(false)
                 setLatestBooks(data.result);
             } catch (error) {
                 console.error("Error fetching latest books:", error);
@@ -22,8 +24,10 @@ const Home = () => {
 
         const fetchPopularBooks = async () => {
             try {
+                setLoading(true)
                 const response = await fetch(`${import.meta.env.VITE_BASE_URL}books/popular`);
                 const data = await response.json();
+                setLoading(false)
                 setPopularBooks(data.result);
             } catch (error) {
                 console.error("Error fetching popular books:", error);
@@ -36,18 +40,27 @@ const Home = () => {
 
     return (
         <div className="container mt-5 pt-5">
-            <h1 className="text-center pt-5"><TbWorldHeart style={{ color: "brown", fontSize: '50px' }} /> Mundo Libros</h1>
-            <Banners />
-            <div className="pt-5">
-                <h3 className="mt-5"> <BiBadgeCheck style={{ color: 'red', fontSize: '50px' }} /> Los más vendidos </h3>
-                <hr />
-                <BookCarousel books={popularBooks} color={`bg-danger`} />
-            </div>
-            <div className="pt-3 pb-5 mb-5">
-                <h3 className="mt-5"> <BiRocket style={{ color: 'darkgreen', fontSize: '50px' }} /> No te pierdas </h3>
-                <hr />
-                <BookCarousel books={latestBooks} color={`bg-success`} />
-            </div>
+            {loading ? (
+                <div className="loader pt-5 pb-5">
+                    <div className="spinner">
+                        <img src="/book.gif" alt="" />
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <h1 className="text-center pt-5"><TbWorldHeart style={{ color: "brown", fontSize: '50px' }} /> Mundo Libros</h1>
+                    <Banners />
+                    <div className="pt-5">
+                        <h3 className="mt-5"> <BiBadgeCheck style={{ color: 'red', fontSize: '50px' }} /> Los más vendidos </h3>
+                        <hr />
+                        <BookCarousel books={popularBooks} color={`bg-danger`} />
+                    </div>
+                    <div className="pt-3 pb-5 mb-5">
+                        <h3 className="mt-5"> <BiRocket style={{ color: 'darkgreen', fontSize: '50px' }} /> No te pierdas </h3>
+                        <hr />
+                        <BookCarousel books={latestBooks} color={`bg-success`} />
+                    </div>
+                </div>)}
         </div>
     )
 }
